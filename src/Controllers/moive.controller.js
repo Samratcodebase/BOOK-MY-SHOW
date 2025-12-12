5a0bca88a1ff49e04d3924f4ee30c45ab348daed
samratcodebase <samratcodebase@gmail.com>
2025-12-12 21:29:22 +0530

    Add MovieController with basic CRUD endpoints

--- src/Controllers/moive.controller.js
+++ src/Controllers/moive.controller.js
@@ -1,4 +1,9 @@
 import Movie from "../Models/movie.model.js";
+/**
+ * @param req :{  }
+ * @param res :{  }
+ * @returns Movie Created
+ **/
 
 const CreateMovies = async (req, res) => {
   try {
@@ -52,4 +57,63 @@ const CreateMovies = async (req, res) => {
   }
 };
 
-export { CreateMovies };
+const deleteMovie = async (req, res) => {
+  try {
+    const movieID = req.params.id;
+
+    if (!movieID) {
+      return res.status(400).json({
+        success: false,
+        message: "Movie ID required",
+      });
+    }
+
+    const movie = await Movie.findByIdAndDelete({ _id: movieID });
+
+    if (!movie) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Movie Deletion SucessFull",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+const getMovies = async (req, res) => {
+  try {
+    const movieName = req.body.movieName;
+
+    const movie = await Movie.find({ movieName: movieName });
+    if (!movie) {
+      return res.status(404).json({
+        success: false,
+        message: "No Matching Movie",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Moive Found",
+      Movie: movie,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+const getMovieByID = async (req, res) => {};
+
+export { CreateMovies, getMovies, deleteMovie };
--- src/Routes/moive.route.js
+++ src/Routes/moive.route.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { CreateMovies } from "../Controllers/moive.controller.js";
+import {
+  CreateMovies,
+  getMovies,
+  deleteMovie,
+} from "../Controllers/moive.controller.js";
 const router = express.Router();
 
 router.post("/movies", CreateMovies);
+router.get("/movies", getMovies);
+router.delete("/movies/:id", deleteMovie);
 
 export default router;
