578711fa1492a85e1c3955dff3be0152b3ca0ed7
samratcodebase <samratcodebase@gmail.com>
2026-01-02 23:46:09 +0530

    Initial Setup for Threatre

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import HealthCheckRouter from "./Routes/healthCheck.route.js";
 import MoiveBookingRouter from "./Routes/moive.route.js";
+import TheatreRouter from "./Routes/theatre .route.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/heath", HealthCheckRouter);
 app.use("/api/v1/mba", MoiveBookingRouter);
+app.use("/api/v1/theatre", TheatreRouter);
 
 export default app;
