12e011c0a27e6248f2fa38a7a0efb7f2ec47cee3
Samratcodebase <samratcodebase@gmail.com>
2026-01-26 19:30:50 +0530

    Refactor : User Registration Logic

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:3001"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
