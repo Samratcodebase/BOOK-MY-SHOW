aecc44012565290cb17dd6531329bf3badca21c8
Samratcodebase <samratcodebase@gmail.com>
2026-01-30 23:52:07 +0530

    Refactor: replace hardcoded error status with constant in updateUser function

--- src/Services/admin.services.js
+++ src/Services/admin.services.js
@@ -1,10 +1,11 @@
 import User from "../Models/user.model.js";
+import { statusCode } from "../Utils/constant.js";
 const updateUser = async (userId, role) => {
   const response = await User.findOne({ _id: userId });
   if (!response) {
     const error = new Error("Invalid User ID");
-    error.statuscode = 400;
-    error.success = falsee;
+    error.statusCode = statusCode.BAD_REQUEST;
+    error.success = false;
   }
 
   response.role = role;
