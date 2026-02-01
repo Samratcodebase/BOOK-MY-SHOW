32fecc5fc9cc6aca7eecafe5ed8d4f1168b76660
Samrat Roy <samratcodebase@gmail.com>
2026-02-01 22:22:28 +0530

    Merge pull request #34 from Samratcodebase/feature/Booking
    Add Validation for Movie Booking

--- src/Routes/booking.route.js
+++ src/Routes/booking.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bookingController from "../Controllers/booking.controller.js";
+import bookingMiddleware from "../Middleware/booking.middleware.js";
 import AuthMiddleware from "../Middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post(
   "/booking",
   AuthMiddleware.isAuthenticated,
+  bookingMiddleware.validateBookingRequest,
   bookingController.createBooking,
 );
 
